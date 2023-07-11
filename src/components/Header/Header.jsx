1c70d6cbea7035bb8db8c3164c32128991ee1f76
Hitesh <111784355+Hitesh-au50@users.noreply.github.com>
2023-07-11 11:11:41 +0530

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,111 +1,114 @@
-import React, { useRef, useEffect, useContext } from 'react'
-import { Container, Row, Button } from 'reactstrap'
-import { Link, NavLink, useNavigate } from 'react-router-dom'
+import React, { useRef, useEffect, useContext } from 'react';
+import { Container, Row, Button } from 'reactstrap';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 import './header.css';
 import { AuthContext } from '../../context/AuthContext';
 
 const nav__links = [
   {
     path: '/home',
-    display: 'Home'
+    display: 'Home',
   },
   {
     path: '/about',
-    display: 'About'
+    display: 'About',
   },
   {
     path: '/tours',
-    display: 'Tours'
-  }
-]
+    display: 'Tours',
+  },
+];
 
 const Header = () => {
-
-  const headerRef = useRef(null)
-  const menuRef = useRef(null)
-  const navigate = useNavigate()
-  const { user, dispatch } = useContext(AuthContext)
+  const headerRef = useRef(null);
+  const menuRef = useRef(null);
+  const navigate = useNavigate();
+  const { user, dispatch } = useContext(AuthContext);
 
   const logout = () => {
-    dispatch({ type: 'LOGOUT' })
-    navigate('/')
-  }
+    dispatch({ type: 'LOGOUT' });
+    navigate('/');
+  };
 
   const stickyHeaderFunc = () => {
     window.addEventListener('scroll', () => {
       if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        headerRef.current.classList.add('sticky__header')
+        headerRef.current.classList.add('sticky__header');
       } else {
-        headerRef.current.classList.remove('sticky__header')
+        headerRef.current.classList.remove('sticky__header');
       }
-    })
-  }
-
+    });
+  };
 
   useEffect(() => {
-    stickyHeaderFunc()
+    stickyHeaderFunc();
 
-    return window.removeEventListener('scroll', stickyHeaderFunc)
-  })
+    return () => window.removeEventListener('scroll', stickyHeaderFunc);
+  }, []);
 
-  const toggleMenu = () => menuRef.current.classList.toggle('show__menu')
+  const toggleMenu = () => menuRef.current.classList.toggle('show__menu');
 
   return (
-    <header className="header" ref={headerRef}>
+    <header className='header' ref={headerRef}>
       <Container>
         <Row>
-          <div className="nav__wrapper d-flex align-items-center justify-content-between">
-
+          <div className='nav__wrapper d-flex align-items-center justify-content-between'>
             {/*============= logo =========== */}
-
-            <div className="logo">
-              <span >APNA</span>  &nbsp;
-              <span> SAFAR</span>
+            <div className='logo'>
+              <span>APNA</span> &nbsp;
+              <span>SAFAR</span>
             </div>
-
             {/*============= logo end =========== */}
 
             {/*============= menu start =========== */}
-
-            <div className="navigation" ref={menuRef} onClick={toggleMenu}>
-              <ul className="menu d-flex align-items-center gap-5">
-                {
-                  nav__links.map((item, index) => (
-                    <li className="nav__item" key={index}>
-                      <NavLink to={item.path} className={navClass => navClass.isActive ? 'active__link' : ""}>{item.display}</NavLink>
-                    </li>
-                  ))
-                }
+            <div className='navigation' ref={menuRef} onClick={toggleMenu}>
+              <ul className='menu d-flex align-items-center gap-5'>
+                {nav__links.map((item, index) => (
+                  <li className='nav__item' key={index}>
+                    <NavLink
+                      to={item.path}
+                      className={(navClass) => (navClass.isActive ? 'active__link' : '')}
+                    >
+                      {item.display}
+                    </NavLink>
+                  </li>
+                ))}
               </ul>
             </div>
-
             {/*============= menu end =========== */}
 
-            <div className="nav__right d-flex align-items-center gap-4">
-              <div className="nav__btns d-flex align-items-center gap-4">
-
-                {
-                  user ? (<>
-                    <h5 className='mb-0 username' style={{ color: '#7b2869' }}>{user.username}</h5>
-                    <Button className='btn btn-dark' onClick={logout}>Logout</Button>
-                  </>) : (<>
-                    <Button className='btn secondary__btn'><Link to='/login'>Login</Link></Button>
-                    <Button className='btn primary__btn'><Link to='/register'>Register</Link></Button>
-                  </>)
-                }
-
-
+            <div className='nav__right d-flex align-items-center gap-4'>
+              <div className='nav__btns d-flex align-items-center gap-4'>
+                {user ? (
+                  <>
+                    <h5 className='mb-0 username' style={{ color: '#7b2869' }}>
+                      {user.username}
+                    </h5>
+                    <Button className='btn btn-dark' onClick={logout}>
+                      Logout
+                    </Button>
+                  </>
+                ) : (
+                  <>
+                    <Button className='btn secondary__btn'>
+                      <Link to='/login'>Login</Link>
+                    </Button>
+                    <Button className='btn primary__btn'>
+                      <Link to='/register'>Register</Link>
+                    </Button>
+                  </>
+                )}
               </div>
 
-              <span className="mobile__menu" onClick={toggleMenu}>
-                <i class="ri-menu-line"></i>
+              <span className='mobile__menu' onClick={toggleMenu}>
+                <i className='ri-menu-line'></i>
               </span>
             </div>
           </div>
         </Row>
       </Container>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
