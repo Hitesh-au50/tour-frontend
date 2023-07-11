78fb190090df8dee09c9063ca0e9e2c47a818306
Hitesh <111784355+Hitesh-au50@users.noreply.github.com>
2023-07-11 11:12:24 +0530

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,3 @@
 
 // export const BASE_URL = 'http://localhost:4000/api/v1'
-export const BASE_URL = 'https://back-mtj9.onrender.com/api/v1'
+export const BASE_URL = 'https://backkkk-vzxx.onrender.com/api/v1'
